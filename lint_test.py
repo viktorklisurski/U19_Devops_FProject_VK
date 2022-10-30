6e082a07d16865d8c737082e51eeef93853cc6ac
viktorklisurski <115937029+viktorklisurski@users.noreply.github.com>
2022-10-30 14:35:25 +0200

    lint actions added
    link actions test

---
+++ lint_test.py
@@ -0,0 +1,10 @@
+import sys
+from pylint import lint
+THRESHOLD = 5
+run = lint.Run(["factorial.py"], do_exit=False)
+score = run.linter.stats["global_note"]
+if score < THRESHOLD:
+    print("Linter failed: Score < threshold value")
+    sys.exit(1)
+sys.exit(0)
+ 
